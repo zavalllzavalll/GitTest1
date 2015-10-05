ab8a4a648a0c8eb2141ffd12d51333a8b2758d05
zavalllzavalll <zavalll@yandex.ru>
2015-10-05 21:15:37 +0300

    Cat class defined. Two cats created and meowed

---
+++ src/Cat.java
@@ -0,0 +1,17 @@
+
+public class Cat 
+{
+	private String name;
+	
+	public Cat(String name)
+	{
+		this.name = name;
+	}
+	
+	public void SayMeow()
+	{
+		System.out.println(this.name + " says: \"Meow!\" ");
+	}
+	
+
+}
--- src/GitTest1.java
+++ src/GitTest1.java
@@ -1,10 +1,14 @@
+import java.awt.font.TextLayout.CaretPolicy;
 
 public class GitTest1 {
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello world");
-
+		Cat catA = new Cat("Vasiliy");
+		Cat catB = new Cat("Shopen");
+		catA.SayMeow();
+		catB.SayMeow();
 	}
 
 }
